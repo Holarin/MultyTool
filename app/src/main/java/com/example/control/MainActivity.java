78cc2eed2c71e08ab45d5bb35143654042356015
Holarin <89611992899@mail.ru>
2020-10-14 23:16:05 +0300

    improving ui

--- app/src/main/java/com/example/control/MainActivity.java
+++ app/src/main/java/com/example/control/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.control;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -11,9 +12,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // remove title
+        //remove title
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
+        //remove nav
+        setContentView(R.layout.calculator);
+
     }
 }
